f7beae3b2555fab44f35463785dab893a1775290
Karen McKinnon <mckinnon@fas.harvard.edu>
2019-02-20 21:49:05 -0700

    added PSD estimate

--- observational_large_ensemble/utils.py
+++ observational_large_ensemble/utils.py
@@ -137,3 +137,105 @@ def create_mode_df(fname):
                    AMO=amo_ts, PDO=pdo_ts, ENSO=enso_ts)
 
     return df
+
+
+def pmtm(x, nw=3, cl=0.95, doplot=False):
+    """Returns Thomson’s multitaper power spectral density (PSD) estimate, pxx, of the input signal, x.
+
+    Slightly modified from Peter Huybers's matlab code, pmtmPH.m
+
+    Parameters
+    ----------
+    x : numpy array
+        Time series to analyze
+    nw : float
+        The time-halfbandwidth product
+    cl : float
+        Confidence interval to calculate and display
+    doplot : logical
+        Indicate whether to create plot of resulting power spectra and uncertainty estimate
+
+    Returns
+    -------
+    P : numpy array
+        PSD estimate
+    s : numpy array
+        Associated frequencies
+    ci : numpy array
+        Associated confidence interval
+
+    """
+    import spectrum
+    import matplotlib.pyplot as plt
+
+    dt = 1  # time step
+    nfft = np.shape(x)[0]
+
+    nx = np.shape(x)[0]
+    k = np.min(np.round(2.*nw), nx)
+    k = int(max(k-1, 1))
+    s = np.arange(0, 1/dt, 1/(nfft*dt))
+
+    # Compute the discrete prolate spheroidal sequences
+    [E, V] = spectrum.dpss(nx, nw, k)
+
+    # Compute the windowed DFTs.
+    Pk = np.abs(np.fft.fft(E*x[:, np.newaxis], nfft, axis=0))**2
+
+    if k > 1:
+        sig2 = np.dot(x[np.newaxis, :], x[:, np.newaxis])[0][0]/nx
+        # initial spectrum estimate
+        P = ((Pk[:, 0] + Pk[:, 1])/2)[:, np.newaxis]
+        Ptemp = np.zeros((nfft, 1))
+        P1 = np.zeros((nfft, 1))
+        tol = .0005*sig2/nfft
+        a = sig2*(1-V)
+
+        while (np.sum(np.abs(P - P1)/nfft) > tol):
+            b = np.repeat(P, k, axis=-1)/(P*V[np.newaxis, :] + np.ones((nfft, 1))*a[np.newaxis, :])
+            wk = (b**2) * (np.ones((nfft, 1))*V[np.newaxis, :])
+            P1 = (np.sum(wk*Pk, axis=-1)/np.sum(wk, axis=-1))[:, np.newaxis]
+
+            Ptemp = np.empty_like(P1)
+            Ptemp[:] = P1
+            P1 = np.empty_like(P)
+            P1[:] = P
+            P = np.empty_like(Ptemp)
+            P[:] = Ptemp
+
+        # Determine equivalent degrees of freedom, see Percival and Walden 1993.
+        v = ((2*np.sum((b**2)*(np.ones((nfft, 1))*V[np.newaxis, :]), axis=-1)**2) /
+             np.sum((b**4)*(np.ones((nfft, 1))*V[np.newaxis, :]**2), axis=-1))
+
+    else:
+        P = np.empty_like(Pk)
+        P[:] = Pk
+        v = 2*np.ones((nfft, 1))
+
+    select = (np.arange(0, (nfft + 1)/2.)).astype('int')
+    P = P[select].flatten()
+    s = s[select].flatten()
+    v = v[select].flatten()
+
+    # Chi-squared 95% confidence interval
+    # approximation from Chamber's et al 1983; see Percival and Walden p.256, 1993
+    ci = np.empty((np.shape(v)[0], 2))
+    ci[:, 0] = 1./(1-2/(9*v) - 1.96*np.sqrt(2/(9*v)))**3
+    ci[:, 1] = 1./(1-2/(9*v) + 1.96*np.sqrt(2/(9*v)))**3
+
+    if doplot:
+        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(12, 8))
+
+        ax.fill_between(s, P*ci[:, 0], P*ci[:, -1], color='lightgray', alpha=0.5, lw=0)
+        ax.plot(s, P)
+        ax.set_xscale('log')
+        ax.set_yscale('log')
+        ax.set_xlim(np.min(s), np.max(s))
+        ax.set_xticks([50**-1, 20**-1, 10**-1, 5**-1, 2**-1])
+        ax.set_xticklabels(['50', '20', '10', '5', '2'])
+        plt.xticks(fontsize=20)
+        plt.yticks(fontsize=20)
+        plt.xlabel('Period (yrs)', fontsize=20)
+        plt.ylabel('Power density', fontsize=20)
+
+    return P, s, ci
