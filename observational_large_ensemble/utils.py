7762448dcc448762d4b8af51edff6bc681ea1bce
Karen McKinnon <mckinnon@fas.harvard.edu>
2019-02-20 18:37:37 -0700

    beginning of EM script

--- observational_large_ensemble/utils.py
+++ observational_large_ensemble/utils.py
@@ -34,3 +34,71 @@ def smooth(data, M):
     valid_indices = np.arange(len(data))[int(M/2):-int(M/2)]
 
     return smoothed, valid_indices
+
+
+def forced_trend(varname, cvdp_loc):
+    """Calculate the global mean, ensemble mean trend across the CESM Large Ensemble.
+
+    This serves to provide the temporal shape for the estimated forced component.
+    The methodology follows Dai et al (2015).
+
+    This script draws upon the output from the CVDP as applied to the CESM1 LE. A tar file containing the
+    CVDP output is available on cheyenne: /gpfs/fs1/collections/cdg/data/cesmLE/CESM-CAM5-BGC-LE/CVDP/
+
+    Parameters
+    ----------
+    varname : str
+        Variable to calculate the EM, GM trend for (tas, pr, slp)
+    cvdp_loc : str
+        Full path and filename for where the CVDP output has been untarred.
+
+    Returns
+    -------
+    gm_em : numpy array
+        Time series of global mean, ensemble mean (across NCAR CESM1) of desired variable
+    gm_em_units : str
+        Units of the GM, EM variable
+    time : numpy array
+        Time associated with above time series
+    time_units : str
+        Units of the time variable
+
+    """
+
+    from glob import glob
+    from netCDF4 import Dataset
+    import numpy as np
+
+    if varname not in list(('tas', 'pr', 'slp')):
+        print('Variable %s not currently supported' % varname)
+        return
+
+    if not cvdp_loc.endswith('/'):
+        cvdp_loc = cvdp_loc + '/'
+
+    if varname == 'slp':
+        # Need to explicitly calculate GM, EM
+        print('Need to code this')
+
+    else:
+        # Can use CVDP output
+        fnames = sorted(glob('%sCESM1-CAM5-BGC-LE_*.cvdp_data.1920-2017.nc' % cvdp_loc))
+
+        cvdp_name = '%s_global_avg_mon' % varname
+
+        nfiles = len(fnames)
+        ds = Dataset(fnames[0], 'r')
+        time = ds['time'][:]
+        time_units = ds['time'].units
+        gm_em_units = ds[cvdp_name].units
+
+        n = len(time)
+        glob_mean = np.empty((nfiles, n))
+        for counter, file in enumerate(fnames):
+            ds = Dataset(file, 'r')
+            glob_mean[counter, :] = ds[cvdp_name][:]
+
+        # Take average across ensemble members
+        gm_em = np.mean(glob_mean, axis=0)
+
+    return gm_em, gm_em_units, time, time_units
