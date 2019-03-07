48edc714ac8ddb7405326938a537e14be44862e3
Karen McKinnon <mckinnon@fas.harvard.edu>
2019-03-06 21:31:23 -0700

    shell script to run components

---
+++ observational_large_ensemble/observational_large_ensemble.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Download data
+source ./scripts/download_data.sh
+
+# Create modes
+python ./scripts/create_surrogate_mode_sets.py
+
+# Fit regression model
+# Model is fit to each month separately, so use parallel for efficiency
+parallel -j12 'python ./scripts/fit_regression.py {1}' ::: $(seq 12)
