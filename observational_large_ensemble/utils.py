f364a0d7bc276b4dc6c6a93663d96eaa96f455a3
Karen McKinnon <mckinnon@fas.harvard.edu>
2019-07-19 14:10:28 -0600

    added obs processing to utils

--- observational_large_ensemble/utils.py
+++ observational_large_ensemble/utils.py
@@ -11,6 +11,8 @@ import xarray as xr
 import matplotlib.pyplot as plt
 import cartopy.feature as cfeature
 import cartopy.crs as ccrs
+import calendar
+from cftime import utime
 
 
 def lowpass_butter(fs, lowcut, order,  data, axis=-1):
@@ -674,3 +676,148 @@ def plot_sst_patterns(lat, lon, beta, ice_loc, modename, savename=None):
     if savename is not None:
         plt.savefig(savename, dpi=300, orientation='landscape', bbox_inches='tight')
         plt.close()
+
+
+def get_obs(this_varname, this_filename, valid_years, mode_lag, cvdp_loc):
+    """Return observational data and associated time series of modes for a given variable.
+
+    Parameters
+    ----------
+    this_varname : str
+        Standard variable name (tas, pr, or slp)
+    this_filename : str
+        Full path to data file
+    valid_years : numpy.ndarray
+        Set of years to pull from file
+    mode_lag : int
+        Number of months to lag the climate variable response from the mode time series
+    cvdp_loc : str
+        Directory containing CVDP output
+
+    Returns
+    -------
+    dsX : xarray.Dataset
+        Dataset containing subset of variable of interest
+    df_shifted : pandas.Dataframe
+        Dataframe with time series of modes and forced component, shifted by "mode_lag" months
+    df : pandas.Dataframe
+        Unshifted version of df_shifted
+
+    """
+
+    # Location of CVDP output
+    modes_fname = '%s/HadISST.cvdp_data.1920-2017.nc' % cvdp_loc  # modes
+
+    # Convert non standard names to standard
+    name_conversion = {'tas': 'temperature', 'pr': 'precip', 'slp': 'prmsl'}
+
+    # Get the forced component
+    # Assume that the global mean trend in sea level is zero
+    if this_varname == 'slp':
+        gm_em, gm_em_units, time, time_units = forced_trend('tas', cvdp_loc)
+        gm_em *= 0
+        gm_em += 1  # will replace constant
+    else:
+        gm_em, gm_em_units, time, time_units = forced_trend(this_varname, cvdp_loc)
+
+    # If using precipitation, need number of days in month to convert units
+    if this_varname == 'pr':
+        gm_time = np.arange(1920 + 0.5/12, 1920 + 1/12*len(time), 1/12)
+        gm_year = np.floor(gm_time)
+        gm_month = np.ceil((gm_time - gm_year)*12)
+        days_per_month = [calendar.monthrange(int(y), int(m))[1] for y, m in zip(gm_year, gm_month)]
+        assert gm_em_units == 'mm/day'  # double check
+        gm_em *= days_per_month
+        gm_em_units = 'mm'
+
+    # Get dataframe of modes
+    df = create_mode_df(modes_fname)
+
+    # Add EM, GM time series to it
+    df = df.assign(F=gm_em)
+
+    # Shift modes in time
+    df_shifted = shift_df(df, mode_lag, ['year', 'month', 'season', 'F'])
+
+    # Subset to valid years
+    subset = np.isin(df_shifted['year'].values, valid_years)
+    df_shifted = df_shifted.loc[subset, :]
+
+    # Load dataset
+    # TODO, switch to xarray
+    ds = Dataset(this_filename, 'r')
+
+    # Load data
+    try:
+        lat = ds['latitude'][:]
+        lon = ds['longitude'][:]
+    except IndexError:
+        lat = ds['lat'][:]
+        lon = ds['lon'][:]
+    try:
+        X = ds[this_varname][:, :, :]
+        X_units = ds[this_varname].units
+    except IndexError:
+        alt_name = name_conversion[this_varname]
+        X = ds[alt_name][:, :, :]
+        X_units = ds[alt_name].units
+
+    # Check unit consistency
+    if this_varname == 'slp':
+        assert X_units == 'Pa'
+    if this_varname == 'pr':
+        assert X_units == 'mm'
+
+    X_time = ds['time'][:]
+    X_time_units = ds['time'].units
+
+    # Code dealing with various time units
+    if X_time_units == 'year A.D.':
+        X_time = X_time.compressed()  # saved as masked array, but no values are masked
+        X_year = np.floor(X_time)
+        X_month = (np.ceil((X_time - X_year)*12)).astype(int)
+
+    else:
+        # For more standard time formats
+        cld = utime(X_time_units)
+        dt = cld.num2date(X_time)
+        X_year = np.array([t.year for t in dt])
+        X_month = np.array([t.month for t in dt])
+
+    if 'climatology' in ds.variables:
+        climo = ds['climatology'][:]
+        # Add climatology to X
+        for counter, this_month in enumerate(X_month):
+            X[counter, ...] += climo[this_month - 1, ...]
+
+    # Permute all data to be time, lat, lon
+    lat_idx = np.where(np.isin(X.shape, len(lat)))[0][0]
+    lon_idx = np.where(np.isin(X.shape, len(lon)))[0][0]
+    time_idx = np.where(np.isin(X.shape, len(X_time)))[0][0]
+
+    X = np.transpose(X, (time_idx, lat_idx, lon_idx))
+    ntime, nlat, nlon = np.shape(X)
+
+    # Subset data
+    subset = np.isin(X_year, valid_years)
+    X = X[subset, :]
+    X_year = X_year[subset]
+    X_month = X_month[subset]
+
+    # Also need to check if our data spans the full valid period
+    subset = np.isin(df_shifted['year'].values, X_year)
+    df_shifted = df_shifted.loc[subset, :]
+
+    # Check that all dimensions look consistent
+    assert len(df_shifted) == np.shape(X)[0]
+
+    # Put into dataset
+    time = pd.date_range(start='%04d-%02d' % (X_year[0], X_month[0]),
+                         freq='M', periods=len(X_year))
+    dsX = xr.Dataset(data_vars={this_varname: (('time', 'lat', 'lon'), X)},
+                     coords={'time': time,
+                             'lat': lat,
+                             'lon': lon},
+                     attrs={'units': X_units})
+
+    return dsX, df_shifted, df
