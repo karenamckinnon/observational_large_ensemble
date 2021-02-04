936a559854008a489b93e15399ea101a3d21c205
Karen McKinnon <mckinnon@fas.harvard.edu>
2021-02-04 09:43:31 -0700

    changed modes and vars to be inputs

--- observational_large_ensemble/params/karen_params_cesm.py
+++ observational_large_ensemble/params/karen_params_cesm.py
@@ -2,11 +2,14 @@
 
 import numpy as np
 
-valid_years = np.arange(1921, 2006)  # skip first year of CESM1-LE, and don't combine with future scenario
+version_name = 'short_pr_noF_noAMV'
+valid_years = np.arange(1966, 2006)  # skip first year of CESM1-LE, and don't combine with future scenario
 cvdp_loc = '/glade/work/mckinnon/CVDP'
 AMO_cutoff_freq = 1/20  # Cut off frequency for Butterworth filter of AMO (1/years)
 mode_lag = 0  # number of months to lag between mode time series and climate response
-workdir_base = '/glade/work/mckinnon/obsLE/parameters'
-output_dir = '/glade/scratch/mckinnon/obsLE/output'
+workdir_base = '/glade/work/mckinnon/obsLE/parameters_v-%s' % version_name
+output_dir = '/glade/scratch/mckinnon/obsLE/output_v-%s' % version_name
 data_dir = '/gpfs/fs1/collections/cdg/data/cesmLE/CESM-CAM5-BGC-LE/atm/proc/tseries/monthly'
 pr_transform = 'boxcox'  # can be boxcox or log
+varnames = ['pr']  # which variables to use to create the synthetic ensemble
+predictors_names = ['constant', 'ENSO', 'PDO_orth']
